edc6904d4230bbba3db419f4ae1f23580f63ed84
Sebastian Ramacher <sramacher@debian.org>
2016-12-10 12:41:16 +0100

    Update list of interesting packages

--- debian/source_vlc.py
+++ debian/source_vlc.py
@@ -7,24 +7,22 @@ Author: Brian Murray <brian@ubuntu.com>
 from apport.hookutils import attach_related_packages
 
 def add_info(report):
-	attach_related_packages(report, [
-		"libaa1",
-		"libgl1-mesa-glx",
-		"libgl1",
-		"libglib2.0-0",
-		"libgtk2.0-0",
-		"libnotify1",
-		"libnotify1-gtk2.10",
-		"libqtcore4",
-		"libqtgui4",
-		"libsdl-image1.2",
-		"libsdl1.2debian",
-		"libx11-6",
-		"libx264-85",
-		"libxcb-keysyms1",
-		"libxcb1",
-		"libxext6",
-		"libxinerama1",
-		"libxv1",
-		"libxxf86vm1",
-		] )
+    attach_related_packages(report, [
+            "libgl1-mesa-glx",
+            "libgl1",
+            "libglib2.0-0",
+            "libgtk2.0-0",
+            "libnotify1",
+            "libnotify1-gtk2.10",
+            "libqt5core5a",
+            "libqt5gui5",
+            "libsdl-image1.2",
+            "libsdl1.2debian",
+            "libx11-6",
+            "libxcb-keysyms1",
+            "libxcb1",
+            "libxext6",
+            "libxinerama1",
+            "libxv1",
+            "libxxf86vm1",
+            ])
